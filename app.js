1b634140fcdec666d36709e6b1f83ae7771bd733
jschrom311 <jschrom311@yahoo.com>
2019-05-02 11:02:28 -0500

    eventlisteners added

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
+//Budget controller
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+//some code
 
 })();
 
+//UI controller
 var UIController = (function() {
     //Some code
 })();
 
+//Global app controller
 var controller = (function(budgetCtrl, UIctrl) {
 
-    var z = budgetCtrl.publicTest(5);
-
-    return {
-        anotherPublic: function () {
-            console.log(z);
-        }
+    var ctrlAddItem = function() {
+        //Get field input data
+        //Add item to budget controller
+        //Add the item to the UI
+        //Calculate the budget
+        //Display budget on UI
     }
+
+    document.querySelector('.add__btn').addEventListener('click', function() {
+
+    document.addEventListener('keypress', function(e) {
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIController);
