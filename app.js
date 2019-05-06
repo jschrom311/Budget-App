31fed24de1970114d464c21b5d1a5c7cb5a9d121
jschrom311 <jschrom311@yahoo.com>
2019-05-06 14:46:37 -0500

    clear input fields

--- app.js
+++ app.js
@@ -95,6 +95,18 @@ var UIController = (function() {
 
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -127,6 +139,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
+        //Clear fields
+        UICtrl.clearFields();
         //Calculate the budget
         //Display budget on UI
     };
