4bf5ee8dcc1289cf49c3cc4cd0427ce9fc6fbf1d
jschrom311 <jschrom311@yahoo.com>
2019-05-07 16:44:17 -0500

    delete item from ui

--- app.js
+++ app.js
@@ -150,6 +150,11 @@ var UIController = (function() {
 
         },
 
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         clearFields: function() {
             var fields, fieldsArr;
             fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
@@ -242,7 +247,9 @@ var controller = (function(budgetCtrl, UICtrl) {
             //Delete item from data structure
             budgetCtrl.deleteItem(type, ID);
             //Delete item from UI
+            UIctrl.deleteListItem(itemID);
             //Update and display new budget
+            updateBudget();
         }
 
 
