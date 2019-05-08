791c5018a479c0d1bd9008d962397cda35eeb451
jschrom311 <jschrom311@yahoo.com>
2019-05-07 21:14:50 -0500

    percentages update

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if(totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome)) * 100;
+        }
+        else {
+            this.percentage = -1;
+        }
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
     };
 
     var Income = function(id, description, value) {
@@ -85,6 +99,19 @@ var budgetController = (function() {
                 data.percentage = -1;
             };
 
+        },
+
+        calculatePercentages: function() {
+            data.allItems.exp.forEach(function(cur){
+                cur.calcPercentage(data.totals.inc);
+            });
+        },
+
+        getPercentages: function() {
+            var allPerc = data.allItems.exp.map(function(cur){
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
 
         getBudget: function() {
@@ -221,8 +248,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var updatePercentages = function() {
         //calculate percentages
+        budgetCtrl.calculatePercentages();
         //read percentages from budget ctrlr
+        var percentages = budgetCtrl.getPercentages();
         //update ui with new percentages
+        console.log(percentages);
     };
 
     var ctrlAddItem = function() {
