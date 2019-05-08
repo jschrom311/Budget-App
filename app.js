a53dc8499b8539e593b65f4350b8e07b303458c9
jschrom311 <jschrom311@yahoo.com>
2019-05-08 13:45:09 -0500

    ui enhancements

--- app.js
+++ app.js
@@ -170,6 +170,12 @@ var UIController = (function() {
 
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getInput: function() {
             return {
@@ -235,11 +241,7 @@ var UIController = (function() {
         displayPercentages: function(percentages) {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
+            
             nodeListForEach(fields, function(current, index) {
                 if(percentages[index] > 0) {
                     current.textContent = percentages[index] + '%';
@@ -262,11 +264,22 @@ var UIController = (function() {
 
         },
 
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType, + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+        },
 
         getDOMstrings: function() {
             return DOMstrings;
         }
-
     };
 
 })();
@@ -287,6 +300,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+        
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+    
     };
 
     var updateBudget = function() {
