26a442e041896dc73db289dd1e94d7f8c7df2010
jschrom311 <jschrom311@yahoo.com>
2019-05-10 13:19:42 -0500

    bug fix

--- app.js
+++ app.js
@@ -194,7 +194,7 @@ var UIController = (function() {
             }
             else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
             //replace placeholder text w/data
             newHtml = html.replace('%id%', obj.id);
